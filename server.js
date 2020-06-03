8c381b1d8557d002b1a3f8acf9509915f7198822
rafiadna <adnan.rfq@gmail.com>
2020-06-03 15:20:32 -0500

    Change Hello World to Hello Cloud

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const { hostname } = require('os'); const http = require('http');
-const message = 'Hello World\n'; const port = 8080;
+const message = 'Hello Cloud\n'; const port = 8080;
 const server = http.createServer((req, res) => { res.statusCode = 200; res.setHeader('Content-Type', 'text/plain'); res.end(message);
 });
   server.listen(port, hostname, () => {
