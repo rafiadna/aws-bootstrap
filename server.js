035b9e0e76596222ad4c2942f7fe1bfc38827522
rafiadna <adnan.rfq@gmail.com>
2020-06-17 11:58:45 -0500

    Add hostname to message

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const { hostname } = require('os'); 
 const http = require('http');
-var hostname = os.hostname();
 const message = 'Hello World from ${hostname()}\n'; 
 const port = 8080;
-
 const server = http.createServer((req, res) => { 
 	res.statusCode = 200; 
 	res.setHeader('Content-Type', 'text/plain'); 
